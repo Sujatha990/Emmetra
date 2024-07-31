6fa0aeeb43f80782fe911d6ca2d6e76a1229c8ba
Sujatha99 <suji84863@gmail.com>
2024-07-31 21:19:12 +0530

    first commit

---
+++ src/app/Automotive.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Image from 'next/image';
+import './Automotive.css'; // Corrected import statement
+
+const Automotive = () => {
+  return (
+    <div className="container">
+      <h1 className="title">Automotive</h1>
+      <figure className="figure">
+        <Image 
+          src="/Images/emmetrasideimg.png" 
+          alt="Automotive Camera Integration" 
+          className="figure-img img-fluid rounded" 
+          width={800} // Adjust width as needed
+          height={450} // Adjust height as needed
+        />
+        <figcaption className="figure-caption">
+          Integrating cameras in vehicles for driver assistance systems,<br/>
+          parking assistance, and 360-degree views.
+        </figcaption>
+      </figure>
+    </div>
+  );
+};
+
+
+export default Automotive;
--- src/app/automative/page.js
+++ src/app/automative/page.js
@@ -1,27 +1,12 @@
-import React from 'react';
-import Image from 'next/image';
-import './Automotive.css'; // Corrected import statement
+import React from 'react'
+import Automotive from '../Automotive'
 
-const AutomotivePage = () => {
+const page = () => {
   return (
-    <div className="container">
-      <h1 className="title">Automotive</h1>
-      <figure className="figure">
-        <Image 
-          src="/Images/emmetrasideimg.png" 
-          alt="Automotive Camera Integration" 
-          className="figure-img img-fluid rounded" 
-          width={800} // Adjust width as needed
-          height={450} // Adjust height as needed
-        />
-        <figcaption className="figure-caption">
-          Integrating cameras in vehicles for driver assistance systems,<br/>
-          parking assistance, and 360-degree views.
-        </figcaption>
-      </figure>
+    <div>
+      <Automotive/>
     </div>
-  );
-};
+  )
+}
 
-
-export default AutomotivePage;
+export default page
